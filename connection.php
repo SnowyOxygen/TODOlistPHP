22b67ffdf769309d80b2fce0d193af18a1065c23
SnowyOxygen <58397639+SnowyOxygen@users.noreply.github.com>
2022-09-12 14:08:06 +0200

    rewriting of todolist

--- connection.php
+++ connection.php
@@ -1,9 +1,38 @@
 <?php
+    $JSONFile = file_get_contents('bdd.json');
+    $JSONData = json_decode($JSONFile, true);
+
+    function GetUsers(){
+        global $JSONData;
+
+        foreach ($JSONData as $key) {
+            echo '<li class="user-element"><button class="user-button">' . $key . "</button></li>";
+        }
+    }
+    function SetUser($username){
+        $_SESSION['username'] = $username;
+    }
+    function CreateUser(){
+        global $JSONData;
+    
+        $userData = [{}];
+        //TODO: Save to file / check structure to append dict element
+        array_push($JSONData, $username => $userData)
+    }
+    function SubmitHandler(){
+        // prevent default
+    }
+
     echo '
-    <form action = "" method="post">
-        <input type="email" placeholder="email@email.com" name="email" required/>
-        <input type="password" placeholder="password" name="password" required/>
-        <input type="submit"/>
-    <form/>
-    '
+        <h1 class="choose">Choissisez un utilisateur.</h1>
+        <div>
+            <ul class="user-list">' . GetUsers() . '</ul>
+        </div>
+        <p class="new-user">Create user:</p>
+        <form action="" method="post">
+            <input type="text" placeholder="username" required maxlength="16"/>
+            <input type="submit"/>
+        </form>
+    ';
+
 ?>
--- index.php
+++ index.php
@@ -1,23 +1,22 @@
-<?php
+<?php 
     session_start();
-
-    if(isset($_POST['email'])){
-        $email = htmlspecialchars($_POST['email']);
-        $_SESSION['email'] = $email;
-    }
+    
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TODO list</title>
+    <title>Todolist</title>
+    <link rel="stylesheet" href="./todo.css">
 </head>
 <body>
     <?php
-        if(isset($_SESSION['email'])){
+        if(in_array('username', $_SESSION)){
             include 'dashboard.php';
-        } else include 'connection.php';
+        } else {
+            include 'connection.php';
+        }
     ?>
 </body>
 </html>
